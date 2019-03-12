d6af18b330444be13f4f7738d1b40c68ec84aaff
Jakub Martin <jakub.wit.martin@gmail.com>
2019-03-12 10:55:14 +0100

    Physical plan materialization may error

--- physical/requalifier.go
+++ physical/requalifier.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/cube2222/octosql/execution"
+	"github.com/pkg/errors"
 )
 
 type Requalifier struct {
@@ -15,6 +16,10 @@ func NewRequalifier(qualifier string, child Node) *Requalifier {
 	return &Requalifier{qualifier: qualifier, source: child}
 }
 
-func (node *Requalifier) Materialize(ctx context.Context) execution.Node {
-	return execution.NewRequalifier(node.qualifier, node.source.Materialize(ctx))
+func (node *Requalifier) Materialize(ctx context.Context) (execution.Node, error) {
+	materialized, err := node.source.Materialize(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't materialize source node")
+	}
+	return execution.NewRequalifier(node.qualifier, materialized), nil
 }

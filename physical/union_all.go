175300b14e3a90bf5a439b16e69eb11e9b44a9b9
woojtek <kuzminskiwojtek@gmail.com>
2019-03-17 18:58:33 +0100

    Union All implementation

---
+++ physical/union_all.go
@@ -0,0 +1,36 @@
+package physical
+
+import (
+	"context"
+
+	"github.com/cube2222/octosql/execution"
+	"github.com/pkg/errors"
+)
+
+type UnionAll struct {
+	first, second Node
+}
+
+func NewUnionAll(left, right Node) *UnionAll {
+	return &UnionAll{first: left, second: right}
+}
+
+func (node *UnionAll) Transform(ctx context.Context, transformers *Transformers) Node {
+	return &UnionAll{
+		first:  node.first.Transform(ctx, transformers),
+		second: node.second.Transform(ctx, transformers),
+	}
+}
+
+func (node *UnionAll) Materialize(ctx context.Context) (execution.Node, error) {
+	firstNode, err := node.first.Materialize(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't materialize first node")
+	}
+	secondNode, err := node.first.Materialize(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't materialize second node")
+	}
+
+	return execution.NewUnionAll(firstNode, secondNode), nil
+}

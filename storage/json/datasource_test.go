ea14c027168fbeb217e9d5e27b57130e416eef21
Jakub Martin <jakub.wit.martin@gmail.com>
2019-07-05 20:54:38 +0200

    Adding json array format option

--- storage/json/datasource_test.go
+++ storage/json/datasource_test.go
@@ -10,16 +10,44 @@ import (
 
 func TestJSONRecordStream_Next(t *testing.T) {
 	tests := []struct {
-		name   string
-		path   string
-		alias  string
-		fields []string
-		want   execution.RecordStream
+		name        string
+		path        string
+		arrayFormat bool
+		alias       string
+		fields      []string
+		want        execution.RecordStream
 	}{
 		{
-			name:  "reading bikes.json - happy path",
-			path:  "fixtures/bikes.json",
-			alias: "b",
+			name:        "reading bikes.json - happy path",
+			path:        "fixtures/bikes.json",
+			arrayFormat: false,
+			alias:       "b",
+			want: execution.NewInMemoryStream(
+				[]*execution.Record{
+					execution.NewRecordFromSlice(
+						[]octosql.VariableName{"b.color", "b.id", "b.ownerid", "b.wheels", "b.year"},
+						[]interface{}{"green", 1.0, 152849.0, 3.0, 2014.0},
+					),
+					execution.NewRecordFromSlice(
+						[]octosql.VariableName{"b.color", "b.id", "b.ownerid", "b.wheels", "b.year"},
+						[]interface{}{"black", 2.0, 106332.0, 2.0, 1988.0},
+					),
+					execution.NewRecordFromSlice(
+						[]octosql.VariableName{"b.color", "b.id", "b.ownerid", "b.wheels", "b.year"},
+						[]interface{}{"purple", 3.0, 99148.0, 2.0, 2009.0},
+					),
+					execution.NewRecordFromSlice(
+						[]octosql.VariableName{"b.color", "b.id", "b.ownerid", "b.wheels", "b.year"},
+						[]interface{}{"orange", 4.0, 97521.0, 2.0, 1979.0},
+					),
+				},
+			),
+		},
+		{
+			name:        "reading bikes.json in array format - happy path",
+			path:        "fixtures/bikes_array.json",
+			arrayFormat: true,
+			alias:       "b",
 			want: execution.NewInMemoryStream(
 				[]*execution.Record{
 					execution.NewRecordFromSlice(
@@ -45,7 +73,7 @@ func TestJSONRecordStream_Next(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ds, err := NewDataSourceBuilderFactory(tt.path)(tt.alias).Materialize(context.Background())
+			ds, err := NewDataSourceBuilderFactory(tt.path, tt.arrayFormat)(tt.alias).Materialize(context.Background())
 			if err != nil {
 				t.Errorf("Error creating data source: %v", err)
 			}

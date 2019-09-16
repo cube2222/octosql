0470e4cb378a51d0bc44378be5eb1b2479b1a147
Jakub Martin <jakub.wit.martin@gmail.com>
2019-09-16 22:30:32 +0200

    Better table valued functions documentation

--- execution/tvf/docgen/main.go
+++ execution/tvf/docgen/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	var body []docs.Documentation
 
-	tvfs := []docs.Documented{&tvf.Range{}}
+	tvfs := []docs.Documented{&tvf.Range{}, &tvf.Tumble{}}
 	for i, el := range tvfs {
 		body = append(body, el.Document())
 		if i != len(tvfs)-1 {
@@ -20,7 +20,14 @@ func main() {
 
 	page := docs.Section(
 		"Table Valued Functions Documentation",
-		docs.Body(body...),
+		docs.Body(
+			append(
+				[]docs.Documentation{
+					docs.Text("Example: SELECT * FROM range(range_start => 1, range_end => 5) r"),
+				},
+				body...,
+			)...,
+		),
 	)
 
 	docs.RenderDocumentation(page, os.Stdout)

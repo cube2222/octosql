67a37b840412141e566cec1527102d2d232fcaec
Wojciech Kuźmiński <kuzminskiwojtek@gmail.com>
2020-01-10 22:24:07 +0100

    little refractor + retractions for avg

--- streaming/aggregate/avg.go
+++ streaming/aggregate/avg.go
@@ -21,14 +21,18 @@ func (agg *Average) AddValue(ctx context.Context, tx storage.StateTransaction, v
 	currentAvgStorage := storage.NewValueState(tx.WithPrefix(currentAvgPrefix))
 	currentAvgNumStorage := storage.NewValueState(tx.WithPrefix(currentAvgNumPrefix))
 
-	currentAvg, err := agg.GetValue(ctx, tx)
+	currentAvgVal, err := agg.GetValue(ctx, tx)
 	if err != nil {
-		return errors.Wrap(err, "couldn't get current count")
+		return errors.Wrap(err, "couldn't get current avg")
 	}
-	currentAvgSlice := currentAvg.AsSlice()
 
-	currentAvgVal := currentAvgSlice[0]
-	currentAvgNum := currentAvgSlice[1]
+	var currentAvgNum octosql.Value
+	err = currentAvgNumStorage.Get(&currentAvgNum)
+	if err == storage.ErrKeyNotFound {
+		currentAvgNum = octosql.MakeInt(0)
+	} else if err != nil {
+		return errors.Wrap(err, "couldn't get current elements number for avg from storage")
+	}
 
 	newScalar := octosql.MakeFloat(float64(currentAvgNum.AsInt() / (currentAvgNum.AsInt() + 1)))
 	currentAvgValScaled := octosql.MakeFloat(currentAvgVal.AsFloat() * newScalar.AsFloat())
@@ -41,7 +45,7 @@ func (agg *Average) AddValue(ctx context.Context, tx storage.StateTransaction, v
 	case octosql.TypeFloat:
 		newComponent = octosql.MakeFloat(value.AsFloat() / float64(currentAvgNum.AsInt()+1))
 	default:
-		return errors.Errorf("unsupported value type passed to sum: %s", valueType)
+		return errors.Errorf("unsupported value type passed to avg: %s", valueType)
 	}
 
 	currentAvgVal = octosql.MakeFloat(currentAvgValScaled.AsFloat() + newComponent.AsFloat())
@@ -61,31 +65,77 @@ func (agg *Average) AddValue(ctx context.Context, tx storage.StateTransaction, v
 }
 
 func (agg *Average) RetractValue(ctx context.Context, tx storage.StateTransaction, value octosql.Value) error {
-	panic("implement me")
+	currentAvgStorage := storage.NewValueState(tx.WithPrefix(currentAvgPrefix))
+	currentAvgNumStorage := storage.NewValueState(tx.WithPrefix(currentAvgNumPrefix))
+
+	currentAvgVal, err := agg.GetValue(ctx, tx)
+	if err != nil {
+		return errors.Wrap(err, "couldn't get current avg")
+	}
+
+	var currentAvgNum octosql.Value
+	err = currentAvgNumStorage.Get(&currentAvgNum)
+	if err == storage.ErrKeyNotFound {
+		currentAvgNum = octosql.MakeInt(0)
+	} else if err != nil {
+		return errors.Wrap(err, "couldn't get current elements number for avg from storage")
+	}
+
+	newScalar := octosql.MakeFloat(float64(currentAvgNum.AsInt() / (currentAvgNum.AsInt() - 1)))
+	currentAvgValScaled := octosql.MakeFloat(currentAvgVal.AsFloat() * newScalar.AsFloat())
+
+	var retractedComponent octosql.Value
+
+	switch valueType := value.GetType(); valueType {
+	case octosql.TypeInt:
+		retractedComponent = octosql.MakeFloat(float64(value.AsInt() / (currentAvgNum.AsInt() - 1)))
+	case octosql.TypeFloat:
+		retractedComponent = octosql.MakeFloat(value.AsFloat() / float64(currentAvgNum.AsInt()-1))
+	default:
+		return errors.Errorf("unsupported value type passed to avg: %s", valueType)
+	}
+
+	currentAvgVal = octosql.MakeFloat(currentAvgValScaled.AsFloat() - retractedComponent.AsFloat())
+	currentAvgNum = octosql.MakeInt(currentAvgNum.AsInt() - 1)
+
+	if currentAvgNum.AsInt() > 0 {
+		err = currentAvgStorage.Set(&currentAvgVal)
+		if err != nil {
+			return errors.Wrap(err, "couldn't set current avg in storage")
+		}
+
+		err = currentAvgNumStorage.Set(&currentAvgNum)
+		if err != nil {
+			return errors.Wrap(err, "couldn't set current elements number for avg in storage")
+		}
+	} else {
+		err = currentAvgStorage.Clear()
+		if err != nil {
+			return errors.Wrap(err, "couldn't set current avg in storage")
+		}
+
+		err = currentAvgNumStorage.Clear()
+		if err != nil {
+			return errors.Wrap(err, "couldn't set current elements number for avg in storage")
+		}
+	}
+
+	return nil
 }
 
 func (agg *Average) GetValue(ctx context.Context, tx storage.StateTransaction) (octosql.Value, error) {
 	currentAvgStorage := storage.NewValueState(tx.WithPrefix(currentAvgPrefix))
-	currentAvgNumStorage := storage.NewValueState(tx.WithPrefix(currentAvgNumPrefix))
 
 	var currentAvg octosql.Value
-	var currentAvgNum octosql.Value
 
 	err := currentAvgStorage.Get(&currentAvg)
 	if err == storage.ErrKeyNotFound {
-		return octosql.MakeTuple([]octosql.Value{octosql.MakeFloat(0), octosql.MakeInt(0)}), nil
+		return octosql.MakeInt(0), nil
 	} else if err != nil {
 		return octosql.ZeroValue(), errors.Wrap(err, "couldn't get current avg from storage")
 	}
 
-	err = currentAvgNumStorage.Get(&currentAvgNum)
-	if err == storage.ErrKeyNotFound {
-		return octosql.MakeTuple([]octosql.Value{octosql.MakeFloat(0), octosql.MakeInt(0)}), nil
-	} else if err != nil {
-		return octosql.ZeroValue(), errors.Wrap(err, "couldn't get current elements number for avg from storage")
-	}
-
-	return octosql.MakeTuple([]octosql.Value{currentAvg, currentAvgNum}), nil
+	return currentAvg, nil
 }
 
 func (agg *Average) String() string {

daaa9d272382bd1423a314a84394630d9c6245c6
JasiekChomiak <JanJakubChomiak@gmail.com>
2020-01-15 19:55:34 +0100

    Added WithPrefix and Subscribe to storage interface

--- streaming/storage/storage.go
+++ streaming/storage/storage.go
@@ -1,15 +1,24 @@
 package storage
 
-import "github.com/dgraph-io/badger/v2"
+import (
+	"context"
+
+	"github.com/dgraph-io/badger/v2"
+	"github.com/pkg/errors"
+)
+
+type KeyValueList = *badger.KVList
 
 type Storage interface {
 	DropAll(prefix []byte) error
 	BeginTransaction() StateTransaction
-	WithPrefix(prefix []byte) StateTransaction
+	WithPrefix(prefix []byte) Storage
+	Subscribe(ctx context.Context, onNotification func() error) error
 }
 
 type BadgerStorage struct {
-	db *badger.DB
+	db     *badger.DB
+	prefix []byte
 }
 
 func NewBadgerStorage(db *badger.DB) *BadgerStorage {
@@ -21,7 +30,7 @@ func NewBadgerStorage(db *badger.DB) *BadgerStorage {
 func (bs *BadgerStorage) BeginTransaction() StateTransaction {
 	//bs.db.DropPrefix()
 	tx := bs.db.NewTransaction(true)
-	return &badgerTransaction{tx: tx, prefix: nil}
+	return &badgerTransaction{tx: tx, prefix: bs.prefix}
 }
 
 func (bs *BadgerStorage) DropAll(prefix []byte) error {
@@ -29,8 +38,22 @@ func (bs *BadgerStorage) DropAll(prefix []byte) error {
 	return err
 }
 
-func (bs *BadgerStorage) WithPrefix(prefix []byte) StateTransaction {
-	tx := bs.db.NewTransaction(true)
-	badgerT := &badgerTransaction{tx: tx, prefix: nil}
-	return badgerT.WithPrefix(prefix)
+func (bs *BadgerStorage) WithPrefix(prefix []byte) Storage {
+	copyStorage := *bs
+	copyStorage.prefix = append(copyStorage.prefix, prefix...)
+
+	return &copyStorage
+}
+
+func (bs *BadgerStorage) Subscribe(ctx context.Context, onNotification func() error) error {
+	cb := func(kv KeyValueList) error {
+		return onNotification()
+	}
+
+	err := bs.db.Subscribe(ctx, cb, bs.prefix)
+	if err != nil {
+		return errors.Wrap(err, "couldn't subscribe to db")
+	}
+
+	return nil
 }

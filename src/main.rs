d1e72ce7e4ec8b6fef46bc6321c959fcb790eab1
Jakub Martin <jakub.wit.martin@gmail.com>
2020-05-18 15:52:52 +0200

    Some basic transformations

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,126 @@ use std::path;
 use std::result::*;
 use std::io::Cursor;
 use arrow::ipc::writer::FileWriter;
-use sled::{ConflictableTransactionError, IVec};
+use arrow::csv;
+use std::io;
+use std::time;
+use sled::{ConflictableTransactionError, IVec, open};
+use arrow::util::pretty::pretty_format_batches;
+use arrow::array::ArrayRef;
+use arrow::datatypes::{Field, Schema};
+use std::sync::Arc;
 
-pub trait PipelineTransform {
-    fn transform(record_batch: RecordBatch) -> RecordBatch;
+pub enum Error {
+    IOError(io::Error),
+    Unexpected,
+}
+
+pub trait Node {
+    fn schema(&self) -> Result<Arc<Schema>, Error>;
+    fn run(&self, produce: &dyn Fn(RecordBatch) -> Result<(), Error>) -> Result<(), Error>;
+}
+
+fn record_print(batch: RecordBatch) -> Result<(), Error> {
+    println!("{}", batch.num_rows());
+    //println!("{}", pretty_format_batches(&[batch]).unwrap());
+    Ok(())
+}
+
+pub struct CSVSource<'a> {
+    path: &'a str
+}
+
+impl<'a> CSVSource<'a> {
+    fn new(path: &'a str) -> CSVSource<'a> {
+        CSVSource { path }
+    }
+}
+
+impl<'a> Node for CSVSource<'a> {
+    fn schema(&self) -> Result<Arc<Schema>, Error> {
+        let file = File::open(self.path).unwrap();
+        let r = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .infer_schema(Some(10))
+            .with_batch_size(8192)
+            .build(file).unwrap();
+        Ok(r.schema())
+    }
+
+    fn run(&self, produce: &dyn Fn(RecordBatch) -> Result<(), Error>) -> Result<(), Error> {
+        let file = File::open(self.path).unwrap();
+        let mut r = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .infer_schema(Some(10))
+            .with_batch_size(8192)
+            .build(file).unwrap();
+        loop {
+            let maybe_rec = r.next().unwrap();
+            match maybe_rec {
+                None => break,
+                Some(rec) => produce(rec),
+            };
+        }
+        Ok(())
+    }
+}
+
+pub struct Projection<'a, 'b> {
+    fields: &'b [&'a str],
+    source: Box<dyn Node>,
+}
+
+impl<'a, 'b> Projection<'a, 'b> {
+    fn new(fields: &'b [&'a str], source: Box<dyn Node>) -> Projection<'a, 'b> {
+        Projection { fields, source }
+    }
+
+    fn schema_from_source_schema(&self, source_schema: Arc<Schema>) -> Result<Arc<Schema>, Error> {
+        let new_schema_fields: Vec<Field> = self.fields
+            .into_iter()
+            .map(|&field| source_schema.index_of(field).unwrap())
+            .map(|i| source_schema.field(i).clone())
+            .collect();
+        Ok(Arc::new(Schema::new(new_schema_fields)))
+    }
+}
+
+impl<'a, 'b> Node for Projection<'a, 'b> {
+    fn schema(&self) -> Result<Arc<Schema>, Error> {
+        let source_schema = self.source.schema()?;
+        self.schema_from_source_schema(source_schema)
+    }
+
+    fn run(&self, produce: &dyn Fn(RecordBatch) -> Result<(), Error>) -> Result<(), Error> {
+        let source_schema = self.source.schema()?;
+        let new_schema = self.schema_from_source_schema(source_schema.clone())?;
+
+        let indices: Vec<usize> = self.fields.into_iter()
+            .map(|&field| source_schema.index_of(field).unwrap())
+            .collect();
+
+        self.source.run(&|batch| {
+            let new_columns: Vec<ArrayRef> = (&indices).into_iter()
+                .map(|&i| batch.column(i).clone())
+                .collect();
+
+            let new_batch = RecordBatch::try_new(
+                new_schema.clone(),
+                new_columns,
+            ).unwrap();
+
+            produce(new_batch)?;
+            Ok(())
+        });
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
 enum QueueError {
     ArrowError(arrow::error::ArrowError),
     SledConflictableTransactionError(sled::ConflictableTransactionError),
-    SerdeJSONError(serde_json::error::Error)
+    SerdeJSONError(serde_json::error::Error),
 }
 
 impl From<arrow::error::ArrowError> for QueueError {
@@ -67,7 +176,7 @@ impl Queue<'_> {
 
         let end_bytes = self.storage.get("end").map_err(ConflictableTransactionError::from)?;
         let end = end_bytes.clone().map(decode_int_from_bytes).unwrap_or(Ok(0))?;
-        let updated_end_bytes = serde_json::to_vec::<i64>(&(end+1))?;
+        let updated_end_bytes = serde_json::to_vec::<i64>(&(end + 1))?;
         self.storage.insert("end", updated_end_bytes).map_err(ConflictableTransactionError::from)?;
 
         let default_key = serde_json::to_vec::<i64>(&0)?;
@@ -83,7 +192,7 @@ impl Queue<'_> {
         let end_bytes = self.storage.get("end").map_err(ConflictableTransactionError::from)?;
         let end = end_bytes.map(decode_int_from_bytes).unwrap_or(Ok(0))?;
 
-        Ok(end-start)
+        Ok(end - start)
     }
 
     // fn pop(&mut self) -> Result<RecordBatch, QueueError> {
@@ -98,14 +207,14 @@ impl Queue<'_> {
 
 fn main() {
     let start_time = std::time::Instant::now();
+    //
+    // let file = File::open("cats.csv").unwrap();
+    // let builder = reader::ReaderBuilder::default().with_batch_size(40000).has_headers(true).infer_schema(Some(16));
+    // let mut cats_reader = builder.build(file).unwrap();
+    //
+    // let batch = cats_reader.next().unwrap().unwrap();
 
-    let file = File::open("cats.csv").unwrap();
-    let builder = reader::ReaderBuilder::default().with_batch_size(40000).has_headers(true).infer_schema(Some(16));
-    let mut cats_reader = builder.build(file).unwrap();
-
-    let batch = cats_reader.next().unwrap().unwrap();
-
-    // let db = sled::open(path::Path::new("testdb")).unwrap();
+    let db = sled::open(path::Path::new("testdb")).unwrap();
 
     //let pipeline_data = db.open_tree("pipeline_data").unwrap();
     //let input_queue = db.open_tree("input_queue").unwrap();
@@ -168,13 +277,44 @@ fn main() {
     // //pretty::print_batches(&vec!(read_batch));
     // println!("{:?}", read_batch.num_columns())
 
-    let db = sled::open(path::Path::new("testdb")).unwrap();
-    let tree = db.open_tree("input_queue").unwrap();
-    let length = tree.transaction::<_,_,QueueError>(|tree| {
-        let input_queue = Queue::new(tree);
-        input_queue.push(&batch)?;
-        let len = input_queue.length()?;
-        Ok(len)
-    });
-    dbg!(length);
+
+    // Queue Test
+    // let db = sled::open(path::Path::new("testdb")).unwrap();
+    // let tree = db.open_tree("input_queue").unwrap();
+    // let length = tree.transaction::<_,_,QueueError>(|tree| {
+    //     let input_queue = Queue::new(tree);
+    //     input_queue.push(&batch)?;
+    //     let len = input_queue.length()?;
+    //     Ok(len)
+    // });
+    // println!("before flush {:?}", start_time.elapsed());
+    // tree.flush();
+    // println!("after flush {:?}", start_time.elapsed());
+    // dbg!(length);
+
+    // let db = sled::open(path::Path::new("testdb")).unwrap();
+    // let tree = db.open_tree("input_queue").unwrap();
+    // tree.set_merge_operator(|key, last_value, to_merge| {
+    //     let cur_value = last_value.map_or(0, |last_value| {
+    //         serde_json::from_slice::<i32>(last_value).unwrap()
+    //     });
+    //     let to_merge = serde_json::from_slice::<i32>(to_merge).unwrap();
+    //     Some(serde_json::to_vec::<i32>(&(cur_value + to_merge)).unwrap())
+    // });
+    // tree.merge("data", serde_json::to_vec::<i32>(&5).unwrap()).unwrap();
+    // tree.merge("data", serde_json::to_vec::<i32>(&4).unwrap()).unwrap();
+    // tree.merge("data1", serde_json::to_vec::<i32>(&4).unwrap()).unwrap();
+    // tree.merge("data", serde_json::to_vec::<i32>(&3).unwrap()).unwrap();
+    // let (v1, v2) = tree.transaction::<_, _, i32>(|db| {
+    //     Ok((serde_json::from_slice::<i32>(db.get("data")?.unwrap().as_ref()).unwrap(), serde_json::from_slice::<i32>(db.get("data1")?.unwrap().as_ref()).unwrap()))
+    // }).unwrap();
+    //
+    // dbg!(v1);
+    // dbg!(v2);
+    //db.with_subscriber()
+
+    let plan: Box<dyn Node> = Box::new(CSVSource::new("cats.csv"));
+    let plan: Box<dyn Node> = Box::new(Projection::new(&["id", "name"], plan));
+    let res = plan.run(&record_print);
+    println!("{:?}", start_time.elapsed());
 }

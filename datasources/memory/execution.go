e4d261de6885a1e6c5342478b47a7cfd399a9374
Jakub Martin <jakub.wit.martin@gmail.com>
2021-01-30 18:59:13 +0100

    Better in memory source.

--- datasources/memory/execution.go
+++ datasources/memory/execution.go
@@ -2,19 +2,36 @@ package memory
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/cube2222/octosql/execution"
 )
 
 type Datasource struct {
-	Records []Record
+	Entries []Entry
+}
+
+type Entry struct {
+	Record         Record
+	WatermarkEntry bool
+	Watermark      time.Time
 }
 
 func (d *Datasource) Run(ctx ExecutionContext, produce ProduceFn, metaSend MetaSendFn) error {
-	for _, record := range d.Records {
-		if err := produce(ProduceFromExecutionContext(ctx), record); err != nil {
-			return fmt.Errorf("couldn't produce record: %w", err)
+	for _, entry := range d.Entries {
+		if !entry.WatermarkEntry {
+			if err := produce(ProduceFromExecutionContext(ctx), entry.Record); err != nil {
+				return fmt.Errorf("couldn't produce record: %w", err)
+			}
+		} else {
+			if err := metaSend(ProduceFromExecutionContext(ctx), MetadataMessage{
+				Type:      MetadataMessageTypeWatermark,
+				Watermark: entry.Watermark,
+			}); err != nil {
+				return fmt.Errorf("couldn't send metadata: %w", err)
+			}
 		}
+
 	}
 	return nil
 }

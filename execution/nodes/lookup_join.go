be77e028cbba3cfb8b702560cc56573fee4236ee
Jakub Martin <jakub.wit.martin@gmail.com>
2021-02-21 21:49:04 +0100

    Parallel lookup join.

--- execution/nodes/lookup_join.go
+++ execution/nodes/lookup_join.go
@@ -19,6 +19,11 @@ func NewLookupJoin(source, joined Node) *LookupJoin {
 }
 
 func (s *LookupJoin) Run(ctx ExecutionContext, produce ProduceFn, metaSend MetaSendFn) error {
+	parallelism := 64
+	sourceMetadataChan := make(chan MetadataMessage)
+	sourceRecordsChan := make(chan Record)
+	semaphore := make(chan struct{}, parallelism)
+
 	if err := s.source.Run(ctx, func(produceCtx ProduceContext, sourceRecord Record) error {
 		ctx := ctx.WithRecord(sourceRecord)
 
@@ -43,5 +48,16 @@ func (s *LookupJoin) Run(ctx ExecutionContext, produce ProduceFn, metaSend MetaS
 	}, metaSend); err != nil {
 		return fmt.Errorf("couldn't run source: %w", err)
 	}
+
+	i := 0
+	lastSent := -1
+	records := make([]Record, parallelism)
+	sourceRecords := make([]Record, parallelism)
+	done := make([]bool, parallelism)
+	joinedRecords := make([][]Record, parallelism)
+	finishedGoroutine := make(chan struct{})
+	// When we receive a message on the done channel,
+	// we send the whole prefix of sourceRecords which are marked as done.
+
 	return nil
 }

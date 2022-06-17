35d2982476cb258d553ab56607fe250c58a5ec17
Jakub Martin <jakub.wit.martin@gmail.com>
2022-06-18 00:37:27 +0200

    Docs continued.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -83,10 +83,10 @@ const config = {
           {
             title: 'Docs',
             items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
+              // {
+              //   label: 'Tutorial',
+              //   to: '/docs/intro',
+              // },
             ],
           },
           {
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/quickstart">
+            Quickstart - 5min ⏱️
           </Link>
         </div>
       </div>

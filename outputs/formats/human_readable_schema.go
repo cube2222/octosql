2ff984f78e37efd92b752f941218e0e01c7fa5e8
Jakub Martin <jakub.wit.martin@gmail.com>
2022-07-17 14:53:33 +0200

    Fix snapshot tests. Only use name without qualifier if it's not a duplicate.

--- outputs/formats/human_readable_schema.go
+++ outputs/formats/human_readable_schema.go
@@ -7,11 +7,23 @@ import (
 )
 
 func withoutQualifiers(fields []physical.SchemaField) []physical.SchemaField {
+	shortName := func(name string) string {
+		if strings.Contains(name, ".") {
+			name = strings.SplitN(name, ".", 2)[1]
+		}
+		return name
+	}
+
+	nameCount := map[string]int{}
+	for i := range fields {
+		nameCount[shortName(fields[i].Name)]++
+	}
+
 	outFields := make([]physical.SchemaField, len(fields))
 	for i := range fields {
 		name := fields[i].Name
-		if strings.Contains(name, ".") {
-			name = strings.SplitN(name, ".", 2)[1]
+		if short := shortName(fields[i].Name); nameCount[short] == 1 {
+			name = short
 		}
 		outFields[i] = physical.SchemaField{
 			Name: name,

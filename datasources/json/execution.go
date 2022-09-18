f5118ac0bbb1db95d7a2083b0d4217c99a5e7300
Jakub Martin <jakub.wit.martin@gmail.com>
2022-09-19 01:11:06 +0200

    Initial JSON parallelization.

--- datasources/json/execution.go
+++ datasources/json/execution.go
@@ -2,7 +2,9 @@ package json
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"runtime"
 	"time"
 
 	"github.com/valyala/fastjson"
@@ -12,6 +14,18 @@ import (
 	"github.com/cube2222/octosql/octosql"
 	"github.com/cube2222/octosql/physical"
 )
+
+type jobIn struct {
+	// jobCtx?
+	line int
+	data []byte
+}
+
+type jobOut struct {
+	line   int
+	record Record
+	err    error
+}
 
 type DatasourceExecuting struct {
 	path   string
@@ -28,31 +42,141 @@ func (d *DatasourceExecuting) Run(ctx ExecutionContext, produce ProduceFn, metaS
 
 	sc := bufio.NewScanner(f)
 	sc.Buffer(nil, 1024*1024)
+	localCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	var p fastjson.Parser
-	for sc.Scan() {
-		v, err := p.ParseBytes(sc.Bytes())
-		if err != nil {
-			return fmt.Errorf("couldn't parse json: %w", err)
-		}
-		if v.Type() != fastjson.TypeObject {
-			return fmt.Errorf("expected JSON object, got '%s'", sc.Text())
-		}
-		o, err := v.Object()
-		if err != nil {
-			return fmt.Errorf("expected JSON object, got '%s'", sc.Text())
-		}
+	inChan := make(chan []jobIn, 64)
+	outChan := make(chan []jobOut, 64)
 
-		values := make([]octosql.Value, len(d.fields))
-		for i := range values {
-			values[i], _ = getOctoSQLValue(d.fields[i].Type, o.Get(d.fields[i].Name))
-		}
+	workers := runtime.GOMAXPROCS(0)
+	for i := 0; i < workers; i++ {
+		go func() {
+			var p fastjson.Parser
 
-		if err := produce(ProduceFromExecutionContext(ctx), NewRecord(values, false, time.Time{})); err != nil {
-			return fmt.Errorf("couldn't produce record: %w", err)
+			for jobs := range inChan {
+				outJobs := make([]jobOut, len(jobs))
+				for i := range outJobs {
+					job := jobs[i]
+					out := &outJobs[i]
+					out.line = job.line
+
+					v, err := p.ParseBytes(job.data)
+					if err != nil {
+						out.err = fmt.Errorf("couldn't parse json: %w", err)
+						continue
+					}
+
+					if v.Type() != fastjson.TypeObject {
+						out.err = fmt.Errorf("expected JSON object, got '%s'", string(job.data))
+						continue
+					}
+					o, err := v.Object()
+					if err != nil {
+						out.err = fmt.Errorf("expected JSON object, got '%s'", string(job.data))
+						continue
+					}
+
+					values := make([]octosql.Value, len(d.fields))
+					for i := range values {
+						values[i], _ = getOctoSQLValue(d.fields[i].Type, o.Get(d.fields[i].Name))
+					}
+
+					out.record = NewRecord(values, false, time.Time{})
+				}
+				select {
+				case outChan <- outJobs:
+				case <-localCtx.Done():
+					return
+				}
+			}
+		}()
+	}
+
+	done := make(chan struct{})
+	linesRead := 0
+	go func() {
+		line := 0
+		batchSize := 32
+		if d.tail {
+			batchSize = 1
+		}
+		batch := make([]jobIn, batchSize)
+		batchIndex := 0
+		for sc.Scan() {
+			data := make([]byte, len(sc.Bytes()))
+			copy(data, sc.Bytes())
+			batch[batchIndex] = jobIn{
+				line: line,
+				data: data,
+			}
+			batchIndex++
+			if batchIndex == batchSize {
+				batchIndex = 0
+				select {
+				case inChan <- batch:
+					linesRead += len(batch)
+				case <-localCtx.Done():
+					close(done)
+					return
+				}
+				batch = make([]jobIn, batchSize)
+			}
+
+			line++
+		}
+		if batchIndex > 0 {
+			batch = batch[:batchIndex]
+			select {
+			case inChan <- batch:
+				linesRead += len(batch)
+			case <-localCtx.Done():
+				close(done)
+				return
+			}
+		}
+		if sc.Err() != nil {
+			panic(sc.Err())
+			// TODO: Przekaż ten błąd na done channel.
+			// TODO: Handle me.
+		}
+		close(done)
+	}()
+
+	var queue []*Record
+	var startIndex int
+	var fileReaderIsDone bool
+produceLoop:
+	for {
+		select {
+		case outJobs := <-outChan:
+			for i := range outJobs {
+				out := outJobs[i]
+				if out.err != nil {
+					return err
+				}
+				for len(queue) <= out.line-startIndex {
+					queue = append(queue, nil)
+				}
+				queue[out.line-startIndex] = &out.record
+				for len(queue) > 0 && queue[0] != nil {
+					record := queue[0]
+					if err := produce(ProduceFromExecutionContext(ctx), *record); err != nil {
+						return fmt.Errorf("couldn't produce: %w", err)
+					}
+					queue = queue[1:]
+					startIndex++
+				}
+			}
+			if fileReaderIsDone && startIndex == linesRead {
+				break produceLoop
+			}
+		// TODO: Back-pressure? Może jakieś tokeny?
+		case <-done:
+			fileReaderIsDone = true
+			done = nil // will block from now on
 		}
 	}
-	return sc.Err()
+	return nil
 }
 
 func getOctoSQLValue(t octosql.Type, value *fastjson.Value) (out octosql.Value, ok bool) {

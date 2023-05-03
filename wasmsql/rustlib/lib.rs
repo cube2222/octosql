99b0607f738a3ad3c89a2d6fb1f783976107d7cb
Jakub Martin <jakub.wit.martin@gmail.com>
2023-05-03 20:48:49 +0200

    Refactor Rust string functions.

--- wasmsql/rustlib/lib.rs
+++ wasmsql/rustlib/lib.rs
@@ -39,17 +39,25 @@ pub extern "C" fn hello() -> u32 {
     Box::into_raw(vec.into_boxed_slice()) as *mut u8 as u32
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn upper(string_header_ptr: u32) -> u32 {
+pub unsafe fn read_wasm_string(string_header_ptr: u32) -> String {
     let ptr = *(string_header_ptr as *const u32);
     let length = *((string_header_ptr +4) as *const u32);
+    String::from_raw_parts(ptr as *mut u8, length as usize, length as usize)
+}
 
-    let out: Vec<_> = String::from_raw_parts(ptr as *mut u8, length as usize, length as usize).to_uppercase().into();
+pub unsafe fn save_wasm_string(string: String) -> u32 {
+    let len = string.len() as u32;
+    let out: Vec<_> = string.into();
     let new_body_ptr = Box::into_raw(out.into_boxed_slice()) as *mut u8 as u32;
 
-    let new_header_ptr = Box::into_raw(Box::new([new_body_ptr, length])) as *mut u8 as u32;
+    let new_header_ptr = Box::into_raw(Box::new([new_body_ptr, len])) as *mut u8 as u32;
 
     new_header_ptr
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn upper(string_header_ptr: u32) -> u32 {
+    save_wasm_string(read_wasm_string(string_header_ptr).to_uppercase())
 }
 
 #[no_mangle]
